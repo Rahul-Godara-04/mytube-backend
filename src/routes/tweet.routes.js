776a40ab61f2bb7a7b0771127e3ab3ccea07c678
Rahul-Godara-04 <rahulgodara63@gmail.com>
2024-02-24 23:48:49 +0530

    Implement tweet routes

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express"
+import {
+    createTweet,
+    deleteTweet,
+    getUserTweet,
+    updateTweet
+} from "../controllers/tweet.controllers.js"
+import { verifyJWT } from "../middleware/auth.middleware.js"
+
+const router = Router();
+router.use(verifyJWT);
+
+router.route("/").post(createTweet);
+router.route("/user/:userId").get(getUserTweet);
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+
+export default router
