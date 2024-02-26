f2970df4d2e231b574b7dd635b3e48d314bb3e76
Rahul-Godara-04 <rahulgodara63@gmail.com>
2024-02-26 19:42:36 +0530

    Implement Subscription Controllers

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,27 @@
+import mongoose, { isValidObjectId } from "mongoose"
+import { User } from "../models/user.models.js"
+import { subscription } from "../models/subscription.models.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    const { channelId } = req.params
+    //TODO: toggle subscription 
+})
+
+const getUserChannelSubscriber = asyncHandler(async (req, res) => {
+    const { channelId } = req.params
+    //TODO: 
+})
+
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subcriberId } = req.params
+
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscriber,
+    getSubscribedChannels
+}
