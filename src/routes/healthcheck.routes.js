6dbeeb03dc37aad7b70a59be771edfcb75ce2db0
Rahul-Godara-04 <rahulgodara63@gmail.com>
2024-03-01 19:30:34 +0530

    Implement healthcheck routes

---
+++ src/routes/healthcheck.routes.js
@@ -0,0 +1,8 @@
+import router, { Router } from "express";
+import { healthcheck } from "../controllers/healthcheck.controllers.js"
+
+const router = Router();
+
+router.route('/').get(healthcheck);
+
+export default router
